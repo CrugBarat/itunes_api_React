ab57153b40df05dde7f9627aae484588191312cb
CrugBarat <craigtbarratt@gmail.com>
2020-06-17 02:54:15 +0100

    formats adta in Song
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,8 +1,23 @@
 import React, {Fragment} from 'react';
 
 const Song = (props) => {
+  if(!props.song) return null;
+
   return (
     <Fragment>
+      <a href={props.song.link[0].attributes.href}>
+        <img src={props.song['im:image'][2].label} alt=""/>
+      </a>
+      <a href={props.song['im:artist'].attributes.href}><p>{props.song.title.label}</p></a>
+      <p>Release: {props.song['im:releaseDate'].attributes.label}</p>
+      <p>Price: {props.song['im:price'].label}</p>
+      <audio controls>
+        <source
+          src={props.song.link[1].attributes.href}
+          type={props.song.link[1].attributes.type}
+          />
+      </audio>
+      <p>{props.song.rights.label}</p>
     </Fragment>
   )
 }
